d279f8cede308fdd0325843ec78b07322a77fd65
OLLYDOTDEV <obell154@gmail.com>
2020-07-22 22:49:17 +1200

    class for smoothing in progress

--- CODE/TESTING/Full_Trigger_Test/Smooth.h
+++ CODE/TESTING/Full_Trigger_Test/Smooth.h
@@ -1,1 +1,1 @@
-
+#include "Arduino.h"
--- CODE/TESTING/Smoothing_class_test/Smoothing_class_test.ino
+++ CODE/TESTING/Smoothing_class_test/Smoothing_class_test.ino
@@ -18,55 +18,86 @@
 // more the readings will be smoothed, but the slower the output will respond to
 // the input. Using a constant rather than a normal variable lets us use this
 // value to determine the size of the readings array.
+
 class AnalogSmooth {
-const int numReadings = 15;        // Used to give amount to data to keep a average of
-    int readings[numReadings];      // the readings from the analog input
-    int readIndex = 0;              // the index of the current reading
-    int total = 0;                  // the running total
-    int average = 0;                // the average
-    int inputPin;                  // Input
-
-
+private:
+#define numReadings 10        // Used to give amount to data to keep a average of
+    int _readings[numReadings];      // the readings from the analog input
+    int _readIndex = 0;              // the index of the current reading
+    int _total = 0;                  // the running total
+    int _average = 0;                // the average
+    int _inputpin;                  // Input
   public:
-    Smooth_setup() {
-      for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-        readings[thisReading] = 0;
-      }
-    }
+    void Smooth_setup() {
+    int Smooth(int inputpin) {
+ };
+ void AnalogSmooth::Smooth_setup(){
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+  readings[thisReading] = 0;
+  }
+}
+  
+}
 
+void setup() {
 
-    Smooth() {
-
-      // subtract the last reading:
-      total = total - readings[readIndex];
-      // read from the sensor:
-      readings[readIndex] = analogRead(inputPin);
-      // add the reading to the total:
-      total = total + readings[readIndex];
-      // advance to the next position in the array:
-      readIndex = readIndex + 1;
-
-      // if we're at the end of the array...
-      if (readIndex >= numReadings) {
-        // ...wrap around to the beginning:
-        readIndex = 0;
-      }
-
-      // calculate the average:
-      average = total / numReadings;
-      // send it to the computer as ASCII digits
-      Serial.println(average);
-
-
-    }
 
 }
 
-    void setup() {
+void loop() {
 
-      Smooth_setup();
-    }
+}
 
-    void loop() {
-      Smooth();
-    }
+/*
+
+http://mypractic.com/lesson-7-classes-in-c-language-for-arduino-button-as-an-object/
+// Define the number of samples to keep track of. The higher the number, the
+// more the readings will be smoothed, but the slower the output will respond to
+// the input. Using a constant rather than a normal variable lets us use this
+// value to determine the size of the readings array.
+class AnalogSmooth(){
+  
+  
+}
+const int numReadings = 15;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+int inputPin;
+
+void setup() {
+
+  pinMode(inputPin, INPUT);
+  // initialize serial communication with computer:
+  Serial.begin(2000000);
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+}
+
+void loop() {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(inputPin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+}
+*/
