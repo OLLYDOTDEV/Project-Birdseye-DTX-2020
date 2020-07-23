38f92176e339dc9b9d3086bd4498b3718d2488ed
Oliver Bell <obell154@gmail.com>
2020-07-23 14:16:37 +1200

    class smoothing untested

--- CODE/TESTING/Smoothing_class_test/Smoothing_class_test.ino
+++ CODE/TESTING/Smoothing_class_test/Smoothing_class_test.ino
@@ -20,23 +20,53 @@
 // value to determine the size of the readings array.
 
 class AnalogSmooth {
-private:
-#define numReadings 10        // Used to give amount to data to keep a average of
-    int _readings[numReadings];      // the readings from the analog input
+  private:
+    #define _numReadings 10        // Used to give amount to data to keep a average of
+    int _readings[_numReadings];      // the readings from the analog input
     int _readIndex = 0;              // the index of the current reading
     int _total = 0;                  // the running total
     int _average = 0;                // the average
-    int _inputpin;                  // Input
   public:
-    void Smooth_setup() {
-    int Smooth(int inputpin) {
- };
- void AnalogSmooth::Smooth_setup(){
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-  readings[thisReading] = 0;
-  }
+    void Smooth_setup();
+    int Smooth(int inputpin);
+    int inputpin;                  // Input
+    
+
+
+
+
+
+};
+
+
+void AnalogSmooth::Smooth_setup(){
+     for (int thisReading = 0; thisReading < _numReadings; thisReading++) {
+     _readings[thisReading] = 0; // sets all array 
+     }
 }
-  
+
+int AnalogSmooth::Smooth(int inputpin) {
+ 
+  // subtract the last reading:
+  _total = _total - _readings[_readIndex];
+  // read from the sensor:
+  _readings[_readIndex] = analogRead(inputpin);
+  // add the reading to the total:
+  _total = _total + _readings[_readIndex];
+  // advance to the next position in the array:
+  _readIndex = _readIndex + 1;
+
+  // if we're at the end of the array...
+  if (_readIndex >= _numReadings) {
+    // ...wrap around to the beginning:
+    _readIndex = 0;
+  }
+
+  // calculate the average:
+  _average = _total / _numReadings;
+  // send it to the computer as ASCII digits
+  Serial.println(_average);
+  return(_average);
 }
 
 void setup() {
@@ -47,6 +77,11 @@ void setup() {
 void loop() {
 
 }
+
+
+
+
+
 
 /*
 
