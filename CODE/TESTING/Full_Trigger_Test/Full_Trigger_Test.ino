04a721f96eaf54f6854a666e117b25f9e35746ea
OLLYDOTDEV <obell154@gmail.com>
2020-07-23 22:57:36 +1200

    FUll Sensor test working with boot info error

--- CODE/TESTING/Full_Trigger_Test/Full_Trigger_Test.ino
+++ CODE/TESTING/Full_Trigger_Test/Full_Trigger_Test.ino
@@ -10,67 +10,95 @@ INFO:
 #include "Smooth.h"
 
 // IR variables
-bool IR_status = 0;
-int IRDATA = 1000 ;
-int IR_threshold = 600 ;
+bool IR_Status = 0;
+int IR_DATA = 0 ;
+int IR_Threshold = 600 ;
 int IR_OUT = A3; // connect ir sensor to arduino pin 2
 // PIR variables
-bool pir_status = 0;
-int pir_out = 5;
+bool Pir_Status = 0;
+bool Pir_Data = 0;
+int Pir_Out = 4;
 // general variables 
-bool Status = 0;
+bool Alert_Status = 0;
 int calibration_trigger = 1; // 1 = calibration mode on | 0 = calibration mode off
+
+
+
+
+
+AnalogSmooth IRsensor; // contruct an object with the smooth class
 void setup() 
 {
-
-
 Serial.begin(9600);
-
+ IRsensor.Smooth_setup(); // set all array values to 0 [see Smooth.h]
 // sensor pins to INPUT
-  pinMode (pir_out, INPUT); 
+  pinMode (Pir_Out, INPUT); 
   pinMode (IR_OUT, INPUT); 
+
+Serial.println("Boot sequence complete initialising main program \n\n\n\n\n\n\n\n\n\n\n\n");
+delay(1000);
 }
 
 void loop()
 {
 
 
-// IR code 
 
-IRDATA = analogRead (IR_OUT);
-if(IRDATA > IR_threshold){
-// IR sensor not triggered no nothing
-IR_status = 0;
-Status = 0;
-}else if(IRDATA < IR_threshold){
+Serial.println("\n\n\n ________________________");
+
+
+
+// IR code 
+IR_DATA = IRsensor.Smooth(IR_OUT);
+
+if(IR_DATA < IR_Threshold){
 // if this if statement activates that means that the sensor has been triggered  
-IR_status = 1;
-Status = 1;
-// then send out alert to relay
+IR_Status = 1;
+}else{
+IR_Status = 0;  
 }
 
 // pir code
-pir_status = digitalRead(pir_out);
-if(pir_status == 1){ // if PIR sensor actavated then set the alert status to active (true)
-Status = 1;
-pir_status=0;
-} else{
-Status = 0;
-}
-// alert system
-if(Status == 1 ){
-// Send alert
-Serial.println("Alarm status: " + Status);
+Pir_Data = digitalRead(Pir_Out);
+if(Pir_Data == 1){ // if PIR sensor actavated then set the alert status to active (true)
+Pir_Status=1;
 }else{
-Serial.println("Alarm status: " + Status);  
+Pir_Status=0;  
 }
 
 
+if(Pir_Status || IR_Status == 1){
+Alert_Status = 1;
+// then send out alert to relay 
+// sensors have been triggered
+}else{
+Alert_Status = 0;
+// no sensor actived no nothing
+}
+
+
+// value debug
+Serial.println("\n");
+Serial.print("IR Data: ");
+Serial.println(IR_DATA);
+Serial.print("PIR Data: ");
+Serial.println(Pir_Data);
+// alert debug
+Serial.println("\n");
+Serial.print("IR Status: ");
+Serial.println(IR_Status);
+Serial.print("PIR Status: ");
+Serial.println(Pir_Status);
+Serial.print("\nAlert_Status: ");
+Serial.println(Alert_Status);
 
 
 
 
 
+
+
+delay(600);
 
 
 }
--- CODE/TESTING/Full_Trigger_Test/Smooth.h
+++ CODE/TESTING/Full_Trigger_Test/Smooth.h
@@ -1,1 +1,64 @@
 #include "Arduino.h"
+
+/*
+  Smoothing
+
+  Reads repeatedly from an analog input, calculating a running average and
+  printing it to the computer. Keeps ten readings in an array and continually
+  averages them.
+
+  created 22 Apr 2007
+  by David A. Mellis  <dam@mellis.org>
+  modified 9 Apr 2012
+  by Tom Igoe
+  convered into class
+  by Oliver Bell
+  This example code is based of http://www.arduino.cc/en/Tutorial/Smoothing
+*/
+
+// Define the number of samples to keep track of. The higher the number, the
+// more the readings will be smoothed, but the slower the output will respond to
+// the input. Using a constant rather than a normal variable lets us use this
+// value to determine the size of the readings array.
+
+class AnalogSmooth {
+  private:
+    #define _numReadings 8        // Used to give amount to data to keep a average of
+    int _readings[_numReadings];      // the readings from the analog input
+    int _readIndex = 0;              // the index of the current reading
+    int _total = 0;                  // the running total
+    int _average = 0;                // the average
+  public:
+    void Smooth_setup();
+    int Smooth(int inputpin);
+    int inputpin;                  // Input
+};
+
+void AnalogSmooth::Smooth_setup(){
+     for (int thisReading = 0; thisReading < _numReadings; thisReading++) {
+     _readings[thisReading] = 0; // sets all array 
+     }
+}
+
+int AnalogSmooth::Smooth(int inputpin) {
+ 
+  // subtract the last reading:
+  _total = _total - _readings[_readIndex];
+  // read from the sensor:
+  _readings[_readIndex] = analogRead(inputpin);
+  // add the reading to the total:
+  _total = _total + _readings[_readIndex];
+  // advance to the next position in the array:
+  _readIndex = _readIndex + 1;
+
+  // if we're at the end of the array...
+  if (_readIndex >= _numReadings) {
+    // ...wrap around to the beginning:
+    _readIndex = 0;
+  }
+
+  // calculate the average:
+  _average = _total / _numReadings;
+  // send it to the computer as ASCII digits
+  return(_average);
+}
