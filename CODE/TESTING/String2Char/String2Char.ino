4de99af05dcec151720f0b2abea2382f9368abc7
OLLYDOTDEV <obell154@gmail.com>
2020-09-03 14:06:51 +1200

    optimisations

--- CODE/TESTING/String2Char/String2Char.ino
+++ CODE/TESTING/String2Char/String2Char.ino
@@ -1,8 +1,10 @@
-
+// based off https://www.geeksforgeeks.org/convert-string-char-array-cpp/ && https://forum.arduino.cc/index.php?topic=550642.0
+// 
+// Make By OLLYDOTDEV
 
 typedef struct
 {
-   String Data = "Char test ";
+   String Data = "Char test";
 
 }
 DataDef;
@@ -18,20 +20,35 @@ char Data[32];
 
 }
 BuffDef;
+
 BuffDef BuffPak;
 
 
-
+#define key 5
 
 
 void setup() {
    Serial.begin(115200);
 }
 void loop() {
-  for (byte i = 0; i <= 9; i++) {
-    BuffPak.Data[i] = DataPak.Data[i]; // String to char Array
-    Serial.println(BuffPak.Data[0]); // Print n'th key of array (Arrays start at 0)
+
+
+
   
+  for (byte i = 0;i <= DataPak.Data.length(); i++) {
+    BuffPak.Data[i] = DataPak.Data[i]; // String to char Array
+
+ 
   }
+
+  
  delay(200); 
+ Serial.print("String Length: ");
+ Serial.println(DataPak.Data.length());
+ 
+     Serial.print("printing ");
+     Serial.print(key);
+    Serial.print("'th key of array: ");
+    Serial.println(BuffPak.Data[key]); // Print n'th key of array (Arrays start at 0)
+
 }
