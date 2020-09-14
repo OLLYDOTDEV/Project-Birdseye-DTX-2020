d842ebca8b15b455ea36d820a3caf7d673648e45
OLLYDOTDEV <obell154@gmail.com>
2020-09-14 21:25:54 +1200

    changed logic to for loop

--- CODE/TESTING/AUGUCLI/aug.cpp
+++ CODE/TESTING/AUGUCLI/aug.cpp
@@ -4,16 +4,12 @@
 
 int main(int argc, char* argv[])
 {
-    // Check the number of parameters
-    if (argc < 2) {
-        // Tell the user how to run the program
-        std::cerr << "Usage: " << argv[0] << " NAME" << std::endl;
-        /* "Usage messages" are a conventional way of telling the user
-         * how to run a program if they enter the command incorrectly.
-         */
-        return 1;
-    }
-    // Print the user's name:
-    std::cout << argv[0] << "says hello, " << argv[1] << "!" << std::endl;
-    return 0;
+for (int i = 1; i < argc; ++i) {
+
+std::cout << "\n" << argv[i] << "\n";
+
 }
+ "\n"
+}
+
+
