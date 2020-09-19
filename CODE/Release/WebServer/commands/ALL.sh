5994bdee9c3d8fbe72121675d8e608ed483aa690
OLLYDOTDEV <obell154@gmail.com>
2020-09-20 01:15:15 +1200

    webserver interface start

---
+++ CODE/Release/WebServer/commands/ALL.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+timeout 5 sudo ps -ef | grep ./../RPI/RPI | grep -v grep | awk '{print $2}' | xargs kill
+sudo ./../RPI/RPI ALL 
---
+++ CODE/Release/WebServer/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo shell_exec('at now -f /projects/Project-Birdseye-DTX-2020/CODE/Release/WebServer/commands/ALL.sh 2>&1');
+
+?>
