6060884578644527b5c9848a0921d3c311fe4fc5
OLLYDOTDEV <obell154@gmail.com>
2020-09-23 15:22:25 +1200

    updated perms | added cp

--- CODE/Release/WebServer/hid.php
+++ CODE/Release/WebServer/hid.php
@@ -3,7 +3,13 @@
 
 <?php
 // https://www.w3schools.com/php/php_superglobals_get.asp
-echo "INFO: " . $_GET['active'];
+$file = $_GET['active'];
+echo "Received: " . $file. "<br>";
+
+echo shell_exec("sudo chmod +x ../HID/* 2>&1");
+echo shell_exec("sudo cp ../HID/".$file." ../HID/Selected.sh");
+
+echo" Copyed ../HID/".$file.", To ../HID/Selected.sh";
 ?>
 </body>
 </html>
